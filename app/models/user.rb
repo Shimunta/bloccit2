70c7319049b6aa0c02fae0e186e63023d13a8a97
Daniel Lisa <danlisa89@gmail.com>
2016-06-16 13:02:06 -0400

    assignment 36 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
+  before_save :fix_name
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
@@ -7,4 +8,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  def fix_name
+    if name
+    self.name = name.split.map{ |name| name.capitalize }.join(' ')
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,11 @@ RSpec.describe User, type: :model do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
     end
+    it "should have a proper name case" do
+      user.name = 'bloccit user'
+      user.save!
+      expect(user.name).to eq "Bloccit User"
+    end
   end
 
   describe "invalid user" do
