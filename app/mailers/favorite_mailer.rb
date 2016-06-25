500b64d3686f91e2f25256ca0462aa6179e115dc
Daniel Lisa <danlisa89@gmail.com>
2016-06-25 18:56:08 -0400

    assignment 44 completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,14 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, cc: "danlisa89@gmail.com", subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<post/#{post.id}@secure-citadel.com>"
+    headers["In-Reply-To"] = "<post/#{post.id}@secure-citadel.com>"
+    headers["References"] = "<post/#{post.id}@secure-citadel.com>"
+
+    @post = post
+
+    mail(to: post.user.email, cc: "danlisa89@gmail.com", subject: "Now following comments on #{post.title}")
+  end
 end
